cc8066658cdc951605abadda5e6ab06c9759add0
Cassandra Liau <cass@Cassandras-MacBook-Pro.local>
2020-09-16 02:54:34 +0800

    article codes + clean up

---
+++ src/containers/ArticleListing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ArticleListing(){
+  return(
+      <section>
+          <h1>Article Listing</h1>
+      </section>
+  );
+}
+
+export default ArticleListing;
