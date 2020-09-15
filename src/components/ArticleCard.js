bfd74d85b8f6f89ae5132265f27eb2b72b55cc2a
Cassandra Liau <cass@Cassandras-MacBook-Pro.local>
2020-09-16 03:25:59 +0800

    article titles added

---
+++ src/components/ArticleCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ArticleCard({articleData}){
+    console.log('articleData', articleData);
+
+    return(
+        <div>
+        <h2>{articleData.title}</h2>
+        </div>
+    );
+}
+
+export default ArticleCard;
--- src/containers/ArticleListing.js
+++ src/containers/ArticleListing.js
@@ -1,10 +1,22 @@
 import React from "react";
 
+//component imports
+import ArticleCard from '../components/ArticleCard';
+
+//data imports
+import Data from '../components/data';
+
 function ArticleListing(){
+  //props= properties = sends data through the flow of the components
   return(
-      <section>
-          <h1>Article Listing</h1>
-      </section>
+      <main>
+          <header>
+          <h1>Articles</h1>
+          </header>
+          {Data.map((article, i) => (
+            <ArticleCard key={i} articleData={article} />
+          ))}
+      </main>
   );
 }
 
