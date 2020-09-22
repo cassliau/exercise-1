347405ef8caec98aba43e4f5054f97e90edd86c8
Cassandra Liau <cass@Cassandras-MacBook-Pro.local>
2020-09-23 04:51:17 +0800

    layout article listing and implement react router:

--- src/containers/Article.js
+++ src/containers/Article.js
@@ -1,6 +1,13 @@
 import React from "react";
 
+import {useParams} from 'react-router-dom';
+
 function Article(){
+  let { id } = useParams();
+  //get what we have in the url as the id
+
+  console.log('id', id);
+
   return(
       <section>
           <h1>Article</h1>
