e35e27e44504d934cbb46a1fd3b82a1b4298f272
Cassandra Liau <cass@Cassandras-MacBook-Pro.local>
2020-09-23 07:22:14 +0800

    added css for article text, header, header wrapper

--- src/containers/Article.js
+++ src/containers/Article.js
@@ -2,15 +2,47 @@ import React from "react";
 
 import {useParams} from 'react-router-dom';
 
-function Article(){
-  let { id } = useParams();
-  //get what we have in the url as the id
+import Data from "../components/data";
 
-  console.log('id', id);
+import { days, months } from "../components/dateValues";
+
+function Article(){
+  let { id } = useParams(); //get what we have in the url as the id
+  
+  const articleData = Data.find((article) => article.id === id);
+
+  if (!articleData) return null; // probably include 404 page...
+
+    const date = new Date(articleData.publishedDate);
+    const dayOfWeek = date.getDay();
+    const calendarDate = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
 
   return(
-      <section>
-          <h1>Article</h1>
+      <section className ='Article'>
+        <header className ="ArticleHeaderWrapper" style = {{backgroundImage:`url('${articleData.image.url}')`}}>
+        <div className='ArticleHeader'>
+            <h1>{articleData.title}</h1>
+            <p>{`${days[dayOfWeek]}, ${months[month]} ${calendarDate}, ${year}`}</p>
+            <p className='ArticleHeaderBlurb'>{articleData.blurb}</p>
+          </div>
+        </header>
+
+        <article className ='ArticleBody'>
+          {articleData.articleText.map((article,i) => {
+          switch(article.type) {
+            case 'p' :
+              return <p key= {i}> {article.data}</p>
+            case 'h2':
+              return <h2 key= {i}> {article.data}</h2>
+            case 'h3':
+              return <h3 key= {i}> {article.data}</h3>
+            default:
+                return null;
+          }
+        })}
+        </article>
       </section>
   );
 }
